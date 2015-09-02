98767d698827d392ac0c2c95268dd344c783da5a
MarinaMeza <kaiodelnorte@yahoo.com.ar>
2015-09-02 11:17:29 -0300

    Primer commit Ejercicio 19

---
+++ Boligrafo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lapìceras
+{
+    class Boligrafo
+    {
+        private ConsoleColor _color;
+        private int _tinta;
+
+        public Boligrafo(string color)
+        { 
+            if(color=="blue")
+            {
+                this._color = ConsoleColor.Blue;
+                this._tinta = 100;
+            }
+            if(color=="red")
+            {
+                this._color = ConsoleColor.Red;
+                this._tinta = 50;
+            }
+        }
+
+        public bool Pintar(int cantidad)
+        {
+            this._tinta = this._tinta - cantidad;
+            return true;   
+        }
+
+        public void Recargar()
+        {
+            this._tinta = 100;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_Nro_19
+{
+    class Ejercicio_19
+    {
+        static void Main(string[] args)
+        {
+            Lapìceras.Boligrafo rojo = new Lapìceras.Boligrafo("red");
+            Lapìceras.Boligrafo azul = new Lapìceras.Boligrafo("blue");
+
+            rojo.Pintar(20);
+        }
+    }
+}
